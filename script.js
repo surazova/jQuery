1765a85a88594d134a60de2acd0ccb881761faf9
Sitora Urazova <sitoraurazova@gmail.com>
2018-08-19 20:38:02 +0000

    Lesson 8 jQuery

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
 /* global $ */
-//lesson 7 
+//lesson 8: Chaining using the "dot" sequence
 
-//next
-$("#contact-methods").next().css({border: "3px solid red"});
+$("#contact-methods").css({border: "2px solid red"}).next().css({border: "2px solid green"}).closest("section").css({border: "2px solid blue"});
 
-//previous
-$("#social-nav").prev().css({border: "3px solid blue"}); //Makes the map image border blue 
-
-//parent
-$(".banner-title").parent().css({border: "3px solid pink"}); //Grabs and element and grabs its parent
-                                                            //You can add "parents" to make it plural and affect all the parents of that element
-
-//children
-$("#social-nav").children().css({border: "3px solid green"});
-
-//find 
-$("#contact").find(".facebook").css({border: "3px solid purple"}); //if facebook exists there, bring it 
-
-//closest
-$("#social-nav").closest(".wrapper").css({border: "3px solid orange"});
+//closest goes up the DOM to find the closest section 
+//All of the statements are grouped together
+//Disclamer: can get messy 
+//Another way to organize: 
+$("#contact-methods").css({border: "2px solid red"})
+    .next().css({border: "2px solid green"})
+    .closest("section").css({border: "2px solid blue"});
