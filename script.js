cd9073d01036ef1a4dd4a03df40dce62b8a27539
Sitora Urazova <sitoraurazova@gmail.com>
2018-08-20 22:09:36 +0000

    Lesson 15 jQuery

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
 /* global $ */
-//Lesson 14: Working with Classes 
+//Lesson 15: Event Binding 
 
+var myLis = $("#points-of-sale li");
 
-$("header .wrapper").removeClass("wrapper");
-$("header > div").addClass("wrapper"); //> means directly under, the direct child under the header 
-
-
-//Use Vanilla JS to toggle the button to open and close 
-var button = $("#lead-banner a"); 
-
-button[0].onclick = function() {
-  $("#points-of-sale").toggleClass("open");
-  return false; 
-    
-};
+myLis.on("click", function() {
+   $(this).css({"background" : "pink"}); 
+   
+   myLis.off("click");
+});
 
 /* 
 
-removeClass() - remves a class from the matched elements
-addClass() - adds a class to the matched elements 
-toggleClass - toggles the class on and off on their matched elements 
-*/
+on() - binds an event to matched elements 
+off() - unbinds event from matched elements 
+
+*/ 
