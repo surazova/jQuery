9edb3f2bf95b59371208de9363ed012933a03c8d
Sitora Urazova <sitoraurazova@gmail.com>
2018-08-20 21:55:00 +0000

    Lesson 13 jQuery

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 /* global $ */
-//Lesson 12: Changing Atributes
+//Lesson 13: Controlling CSS
 
-$("#contact img").removeAttr("alt");
-$("#contact img").attr("alt", "location"); //sets the attribute 
-console.log($("#contact img").alt("alt")); //Reads the attribute 
+console.log($("#social-nav").css("position")); //checks the value of a certain property already given to that element 
+$("#social-nav").css("top", "200px").css("left", "100px"); //Chaining the values, but it gets messy. Another method is to pass an object 
 
-/*
+$("#social-nav").css({
+    "top" : "-400px",
+    "left" : "150px",
+    "opacity" : "0.5",
+    "border-top" : "4px solid red"
+});
 
-.removeAttr() - removes an attibute completely 
-.attr() - can read or set any attribute 
-
-*/
