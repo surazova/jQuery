2109a800d8f82841c2d70089ec684b2084ba6264
Sitora Urazova <sitoraurazova@gmail.com>
2018-08-20 21:43:23 +0000

    Lesson 11 jQuery

--- script.js
+++ script.js
@@ -1,35 +1,15 @@
 /* global $ */
-//Lesson 10: Wrap and Unwrap elements using jQuery
+//Lesson 11: Removing Content
 
-//$("section").wrap("<div>");
-//$("section").unwrap();
-//$("section").wrapAll("<div>");
+$(".button").empty();
+$("#points-of-sale").empty();
+$(".button").remove();
+$("#contact img").remove();
 
 
 /*
-wrap() - wraps all matched elements individually 
-unwrap() - unwraps all matched elements 
-wrapAll() - wraps all elements combined with 1 single element 
+
+.empty() - empties the inner HTML of an element 
+.remove() - removes the element completely
+
 */
-
-//Examples 
-
-var wrapper = "<div class='wrapper'>";
-var button = $(".button");
-var wrapped = true;
-
-//Vanilla JS
-button[0].onclick = function() {
-    
-    if(wrapped){
-        $("section").unwrap();
-        wrapped = false; 
-        button.text("Wrap");
-    } else {
-        $("section").wrapAll(wrapper);
-        wrapped = true;
-        button.text("Unwrap");
-    }
-};
-
-//You can use this to make a video full screen on your website. 
