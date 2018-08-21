ad3b5b453fb742ba33a2cff6a0371986cf8991a9
Sitora Urazova <sitoraurazova@gmail.com>
2018-08-21 17:54:19 +0000

    Lesson 19 jQuery

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 /* global $ */
-//Lesson 18: JQuery Event Object 
+//Lesson 19: jQuery Animations 
 
-// * Universal selector
-$(document).ready(function(){
 
-    $("*").on("click", function(e){
-        
-        console.log(e.target);
-        console.log("The event type is: " + e.type);
-        console.log("X co-ordinate of the event is: " + e.pageX);
-        console.log("Y co-ordinate of the event is: " + e.pageY);
-       //X and Y is useful to see where users clicked on the page, might be helpful if you have a game
-        e.stopProgation();
-        
+//We can only animate properties that have a numerical value 
+//We can't change the font color, etc. 
+$(document).ready(function() {
+    
+    function complete() {
+        alert("Animation Complete");
+    }
+    $("section > h2").on("click", function(){
+      
+      $(this).animate({"width" : "500px", "height" : "100px"}, 100, "linear", complete); //the 100 is the second parameter, tells you how fast to animate something. The third parameter, linear sets the swing of the animation. The fourth is a c
+      //callback function, that fires when something is complete. 
     });
 });
+
+//Might be useful if you have a shopping website, you can animate the object to the cart and give an alert that the item was added 
