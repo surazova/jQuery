b74513cc11e0b737563559f62db4b6fac300ee10
Sitora Urazova <sitoraurazova@gmail.com>
2018-08-21 22:58:44 +0000

    Lesson 24 jQuery

--- script.js
+++ script.js
@@ -1,29 +1,13 @@
 
 /* global $ */
-//Lesson 23: Animation Example 1: Quote Fader 
+//Lesson 24: Sliding Animation Example
 
 $(document).ready(function() {
     
-    var allQuotes = $("blockquote");
-    var currentQuote = 0;
+    var items = $("#points of sale li");
     
-    function changeQuote() {
-        
-        $(allQuotes[currentQuote]).fadeOut(200, function() {
-            
-      
-        
-        if(currentQuote == allQuotes.length - 1) {
-            currentQuote = 0;
-        } else {
-            currentQuote++;
-        }
-        
-        $(allQuotes[currentQuote]).fadeIn(200);
+    items.on("click", function() {
+        $(this).find("p").slideToggle(500);
     });
-    }
-    
-    var quoteTimer = setInterval(changeQuote, 3000); //You need to call the function 
 });
-
-//If the quote is set to 2, then reset it back to the first one, 0 
+ 
