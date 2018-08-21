b364fb25d8f9121676c9fa8dc1a20ac8d3eeb666
Sitora Urazova <sitoraurazova@gmail.com>
2018-08-21 17:45:48 +0000

    Lesson 18 jQuery

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 /* global $ */
-//Lesson 17: Document ready and window load events 
+//Lesson 18: JQuery Event Object 
 
-//When you must place your script tags in the front set this: 
-$(document).on("ready", function() {   //When the document is ready, then fire or bind your events 
-    
-});
+// * Universal selector
+$(document).ready(function(){
 
-//or you can use
-$(function(){
-    
-});
-
-//or
-$(window).on("load", function(){ //Once everything has fully loaded on the page, then fire up the code
-    
+    $("*").on("click", function(e){
+        
+        console.log(e.target);
+        console.log("The event type is: " + e.type);
+        console.log("X co-ordinate of the event is: " + e.pageX);
+        console.log("Y co-ordinate of the event is: " + e.pageY);
+       //X and Y is useful to see where users clicked on the page, might be helpful if you have a game
+        e.stopProgation();
+        
+    });
 });
